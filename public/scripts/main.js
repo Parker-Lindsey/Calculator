9646206fa73c065b567de6ccb46173822e226d2d
Parker <plindsey1496@gmail.com>
2019-09-15 21:46:12 -0500

    Started working on functionality for calculator

---
+++ public/scripts/main.js
@@ -0,0 +1,13 @@
+//Hit button make number
+function display(input) {
+  var input = input;
+  var screen = document.getElementById('display').innerHTML;
+
+  screen = screen + input;
+}
+
+//Hit button hold onto operation
+
+//Hit button solve equation
+
+//Display it all
