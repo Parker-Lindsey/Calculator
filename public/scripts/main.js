7489d4c1a0f2f94b29bc5e284957f33a7e664e81
Parker <plindsey1496@gmail.com>
2019-09-18 14:05:38 -0500

    Got basic operations working for calculator

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,13 +1,68 @@
-//Hit button make number
-function display(input) {
-  var input = input;
-  var screen = document.getElementById('display').innerHTML;
+var currentOperator; //Holds what operator you hit
+var firstValue; //Holds the number before the operator
+var secondValue; //Holds the number after the operator
 
-  screen = screen + input;
+
+//Gives the current number displayed on the screen
+function screenDisplay() {
+  var screenDisplay = document.getElementById('display').innerHTML;
+  return screenDisplay;
 }
 
-//Hit button hold onto operation
+//Hit button make number display
+function display(input) {
+  document.getElementById('display').innerHTML = "";
+  var i = input;
+  var display = document.getElementById('display').innerHTML + i;
+  document.getElementById('display').innerHTML = display;
+  var sd = screenDisplay();
+}
+
+//Hit operator button
+function operator(input) {
+  var operator = input;
+  getFirstValue();
+  currentOperator = operator;
+  document.getElementById('display').innerHTML = "";
+}
+
+//Save first value into variable
+function getFirstValue(){
+  firstValue = screenDisplay();
+  console.log(firstValue);
+}
+
+//Save second value into variable
+function getSecondValue(){
+  secondValue = screenDisplay();
+  console.log(secondValue);
+}
 
 //Hit button solve equation
+function answer() {
+  var answer;
 
-//Display it all
+  getSecondValue();
+
+  console.log(currentOperator);
+
+  if(currentOperator == '*') {
+    answer = +firstValue * +secondValue;
+  } else if(currentOperator == '/') {
+    answer = +firstValue / +secondValue;
+  } else if(currentOperator == '-') {
+    answer = +firstValue - +secondValue;
+  } else if(currentOperator == '+'){
+    answer = +firstValue + +secondValue;
+  }
+  document.getElementById('display').innerHTML = answer;
+  console.log(answer);
+}
+
+//All Clear
+function allClear() {
+  document.getElementById('display').innerHTML = "0";
+  firstValue = 0;
+  secondValue = 0;
+  currentOperator = "";
+}
